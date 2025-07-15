09b4812119c1a13d71a2c950bdcc351cf0538779
pattmapoh <pattmapoh32@gmali.com>
2025-07-15 10:33:19 +0700

    pattmapoh

---
+++ webpage/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(req):
+    
+    return render(req, 'index.html')
+
+def about(req):
+    
+    return render(req, 'about.html')
+
+def contact(req):
+    
+    return render(req, 'contact.html')
+
