5d7e287b51d21c4d356fdc40958e7b95715f6d9f
pattmapoh <pattmapoh32@gmali.com>
2025-07-15 11:47:35 +0700

    C

--- django_deploy/settings.py
+++ django_deploy/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'statics')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.Path.joinpath(BASE_DIR, 'media')
